fce2785531c2b79973ac241fb68f8f3e3e1ef419
Dani de la Cruz <5173869+delacruz-dev@users.noreply.github.com>
2020-08-10 23:34:56 +0200

    Remove stuff from codesandbox.io

--- src/services/pokemons.ts
+++
@@ -1,7 +0,0 @@
-import { Pokemon } from "./types";
-
-export async function getPokemons(): Promise<Pokemon[]> {
-  const response = await fetch("https://pokeapi.co/api/v2/pokemon");
-  const { results } = await response.json();
-  return results;
-}
